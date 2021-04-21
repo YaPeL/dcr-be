e20b24836480bb20154d29b9e74ae523ef0237bf
YaPeL <elmaildejapel@gmail.com>
2021-04-21 16:16:30 -0300

    simplify the code, make better docs

--- app/main.py
+++ app/main.py
@@ -20,35 +20,21 @@ if os.getenv('ENVIRONMENT', 'PROD') != 'PROD':
 app = FastAPI()
 
 
-async def get_payload(request: Request) -> dict:
-    try:
-        payload = await request.json()
-    except JSONDecodeError:
-        raise HTTPException(status_code=400, detail="Bad request: missing body")
-    return payload
-
-
 @app.post("/geo/reverse")
-async def reverse(request: Request):
-    payload = await get_payload(request)
-    try:
-        d = Direction(**payload)
-    except ValidationError as e:
-        raise HTTPException(status_code=400, detail=f"Bad request: {e.json()}")
+async def reverse(direction: Direction):
     api_key = os.getenv('GMAP_API_KEY', None)
     async with Client(api_key) as client:
-        response = await client.geocode(str(d))
+        response = await client.geocode(str(direction))
         if not response:
             raise HTTPException(status_code=404, detail=f"Direction not found: {str(d)}")
         return JSONResponse(content=response[0]["geometry"]["location"])
 
 
 @app.post("/geo/nearby")
-async def nearby(request: Request):
-    payload = await get_payload(request)
+async def nearby(point: Point):
     response = {"markers": []}
     try:
-        dcrp = DCRPoint(point=Point(**payload))
+        dcrp = DCRPoint(point=point)
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=f"Bad request: {e.json()}")
     for point in dcrp.nearby_points():
